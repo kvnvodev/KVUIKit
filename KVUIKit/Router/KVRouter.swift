285fcc6a226e5b1438d1d7dda3818deb12c95c25
kvnvodev <kvnvo.dev@gmail.com>
2018-10-20 23:57:22 +0700

    Add simple router.

---
+++ KVUIKit/Router/KVRouter.swift
@@ -0,0 +1,17 @@
+//
+//  KVRouter.swift
+//  KVUIKit
+//
+//  Created by Khanh Vo on 10/20/18.
+//  Copyright © 2018 kvnvo. All rights reserved.
+//
+
+import UIKit
+
+class KVRouter: NSObject {
+
+}
+
+func KVNavigate(to page: String) {
+    print("KVNavigate to: \(page)")
+}
