df1266e32a9b6839a9b6bfb21ce897d2f53a58f5
kvnvo.dev <kvnvo.dev@gmail.com>
2018-10-20 23:05:01 +0700

    Initialize KVUIKit framework.

---
+++ KVUIKit/KVUIKit.h
@@ -0,0 +1,19 @@
+//
+//  KVUIKit.h
+//  KVUIKit
+//
+//  Created by Khanh Vo on 10/20/18.
+//  Copyright © 2018 kvnvo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVUIKit.
+FOUNDATION_EXPORT double KVUIKitVersionNumber;
+
+//! Project version string for KVUIKit.
+FOUNDATION_EXPORT const unsigned char KVUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVUIKit/PublicHeader.h>
+
+
